fe1f8a3b268ec1efe0a3c2bf9753ae0d6d54ae64
shunal <stevehunal@gmail.com>
2020-03-23 21:40:33 -0400

    fixed weird node modules and dockerized front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 // import React from 'react';
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import CreateDrill from './components/CreateDrill';
 import './App.css';
