f9b6897bd4d0a912e9ce346a3083c5c5a3c6b616
Steven Hunal <stevehunal@gmail.com>
2022-10-26 16:03:35 -0400

    upgraded gems

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -2,24 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_223718) do
-
+ActiveRecord::Schema[7.0].define(version: 2019_12_23_223718) do
   create_table "drills", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.string "description"
     t.string "category"
     t.string "drawing"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_drills_on_user_id"
   end
 
@@ -32,8 +31,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_223718) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,8 +41,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_223718) do
     t.string "image"
     t.string "facebookid"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
